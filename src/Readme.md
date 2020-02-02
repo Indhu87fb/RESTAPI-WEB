8735e690ab5a63fc5b05145aae10bd123f691d34
Indhu87fb <46392460+Indhu87fb@users.noreply.github.com>
2020-02-02 12:42:32 +0530

    RestAPI get and post
    get and postmethod

--- src/Readme.md
+++ src/Readme.md
@@ -3,3 +3,11 @@ TEmplate Literals/String
 json({ students }); this is like json({ students:"students" });
 object-key:value same value - so we are using as json({ students });instead of json({ students:"students" });
 this is called "object shorthand notation"
+
+truth,falsy value
+object restructuring
+casting types
+
+creating routes
+creating and using middlewares
+bodyParser
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const students = require("./models/studentDb");
+const bodyParse = require("body-parser");
 const app = express();
+
+app.use(bodyParse.json());
 
 app.get("/", (request, response) => {
   response.send("hello!!!");
@@ -19,7 +22,25 @@ app.get("/studentDb", (req, res) => {
   // res.send("<h1>Hey</h1>")
 });
 app.post("/studentDb", (req, res) => {
-  res.send("post method called");
+  if (req.body.firstName && req.body.lastName) {
+    students.push(req.body);
+    res.status(200).json({ message: "student created successful" });
+  } else {
+    res.status(400).send("bad request");
+  }
+  // console.log(req.body);
+  // res.send("post method");
+  //res.status(200).json({message:"student created successful"});
+});
+
+app.get("/studentDb/:id", (req, res) => {
+  //const studentId==req.params;
+  const { id = "" } = req.params;
+  const requiredstudent = students.find(student => {
+    if (parseInt(id) === student.id) return true;
+    else return false;
+  });
+  res.send(requiredstudent);
 });
 
 const server = app.listen(8080, () => {
